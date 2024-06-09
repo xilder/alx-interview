2f5cc45e3edd43d68d8f173d9f960e99e50fb27e
xilder <olabowaleayobami@gmail.com>
2024-06-10 00:36:51 +0100

    ua

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,13 +6,14 @@ def canUnlockAll(boxes):
     """function to check if all boxes can be opened"""
     my_set = {0}
     my_keys = boxes[0]
-    
+
     while my_keys and len(my_set) < len(boxes):
         key = my_keys.pop()
 
         if key in my_set or key >= len(boxes):
             continue
+
         my_set.add(key)
         my_keys.extend(boxes[key])
-        
+
     return len(my_set) == len(boxes)
