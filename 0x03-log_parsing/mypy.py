711493b289f21fafc7d56db5636a6c3dfd9fb6b8
xilder <olabowaleayobami@gmail.com>
2024-06-24 09:19:22 +0100

    adds task 0

---
+++ 0x03-log_parsing/mypy.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+log_parsing exercise
+"""
+import re
+import sys
+
+
+def print_log(codes, file_size):
+    """prints log stored"""
+    print(f"File size: {file_size}")
+    sorted_keys = sorted(codes.keys())
+    for key in sorted_keys:
+        print(f"{key}: {codes[key]}")
+
+
+def parse_log():
+    """parses log from stdin"""
+    codes = {}
+    i = 0
+    file_size = 0
+    try:
+        for line in sys.stdin:
+            i += 1
+            intel = line.split(" ")
+            try:
+                code = (intel[-2])
+                size = int(intel[-1])
+                if len(code) == 3:
+                    code = int(code)
+                    codes[code] = codes.get(code, 0) + 1
+                    file_size += int(size)
+            except (IndexError, ValueError):
+                pass
+            if i == 10:
+                print_log(codes, file_size)
+                i = 0
+    except KeyboardInterrupt:
+        print_log(codes, file_size)
+
+
+if __name__ == "__main__":
+    parse_log()
