bc273030a0a32976d6f407f7621213dfd683a99e
xilder <olabowaleayobami@gmail.com>
2024-06-24 02:02:31 +0100

    adds task 0

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+log_parsing exercise
+"""
+import re
+import sys
+
+
+def print_log(codes, file_size):
+    """prints log stored"""
+    print(f"File size{file_size}")
+    for code, freq in codes.items():
+        print(f"{code}: {freq}")
+
+
+def parse_log():
+    """parses log from stdin"""
+    codes = {}
+    i = 0
+    file_size = 0
+    try:
+        for line in sys.stdin():
+            i += 1
+            intel = line.split(" ")
+            try:
+                code = int(intel[-2])
+                size = int(intel[-1])
+                codes[code] = codes.get(code, 0) + 1
+                file_size += int(size)
+            except (IndexError, ValueError):
+                pass
+            if i == 10:
+                print_log(codes, file_size)
+    except KeyboardInterrupt:
+        print_log(codes, file_size)
+
+
+if __name__ == "__main__":
+    parse_log()
---
+++ 0x03-log_parsing/README.md
@@ -0,0 +1,1 @@
+ALX - 0x03-log_parsing
