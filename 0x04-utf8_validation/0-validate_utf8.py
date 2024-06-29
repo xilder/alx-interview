b0689ff86f4cc4836789c2ea6070db84b83d0499
xilder <olabowaleayobami@gmail.com>
2024-06-29 23:10:52 +0100

    update task 0: fix number of bytes for algorithm check

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,16 +6,16 @@ def validUTF8(data):
     """utf8 validation algorithm"""
     length = 0
     for b in data:
-        b = b & 0b11111111
+        # b = b & 0b11111111
         if length == 0:
             if b >> 7 == 0:
                 continue
             elif b >> 5 == 6:
-                length = 2
+                length = 1
             elif b >> 4 == 14:
-                length = 3
+                length = 2
             elif b >> 3 == 30:
-                length = 4
+                length = 3
         else:
             if b >> 6 != 2:
                 return False
